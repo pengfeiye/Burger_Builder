1bd23fe9a8145c0b27c69c32809a715886bd8176
pengfei <pengfeiye1994@gmail.com>
2018-03-30 22:26:04 -0700

    Burger State Setup

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+  const ingredients = Object.keys(props.ingredients).map(igKey => {
+    console.log([...Array(props.ingredients[igKey])])
+    console.log(igKey)
+    return [...Array(props.ingredients[igKey])].map((_,i)=>{
+      return <BurgerIngredient key={igKey + i} type={igKey}/>;
+    })
+  })
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {ingredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   )
