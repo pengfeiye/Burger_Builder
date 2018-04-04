f5598d583f2341a478d5738969d18d4d8fd3d2cd
pengfei <pengfeiye1994@gmail.com>
2018-04-04 16:26:06 -0700

    logo added to toolbar

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,10 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 //importing img dynamically
-import burgerLogo from '../../assets/images/burgerLogo'
+import burgerLogo from '../../assets/images/burger-logo.png'
+import classes from './Logo.css'
 
 const logo = (props) => (
-      <div>
+      <div className={classes.Logo}>
         <img src={burgerLogo} alt="Super Burger"/>
       </div>
 )
