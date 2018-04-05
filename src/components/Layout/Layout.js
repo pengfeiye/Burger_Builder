938317cef3e18ecf8b0205de2c152b9f6e92d13a
pengfei <pengfeiye1994@gmail.com>
2018-04-04 17:13:53 -0700

    side drawer component created

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import styles from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const Layout = (props) => (
   <Aux>
-    <div>
-      <Toolbar />
-    </div>
+    <Toolbar />
+    <SideDrawer />  
     <main className={styles.Content}>
     {props.children}
     </main>
